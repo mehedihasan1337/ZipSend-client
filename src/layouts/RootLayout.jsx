05086bf6496d301c7877bf8c63a5f67183ffc045
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2025-10-16 23:15:49 +0600

    addes navberor footer

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navber from '../pages/shared/Navber';
+import Footer from '../pages/shared/Footer';
 
 const RootLayout = () => {
     return (
         <div>
+            <Navber></Navber>
               <Outlet></Outlet>
+              <Footer></Footer>
         </div>
     );
 };
---
+++ src/pages/shared/ZipSendLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/ZipSendLogo.png'
+
+const ZipSendLogo = () => {
+    return (
+        <div>
+            <img className='w-16' src={logo} alt="" />
+        </div>
+    );
+};
+
+export default ZipSendLogo;
