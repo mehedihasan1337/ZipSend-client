ff4767b78126250ff5e0710731fd78959f499202
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2025-10-27 23:15:26 +0600

    added google login btn

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
     const { register,
@@ -13,34 +14,41 @@ const Login = () => {
     }
     return (
         <div>
-            <form onSubmit={handleSubmit(oneSubmit)}>
-                <fieldset className="fieldset">
-                    <label className="label">Email</label>
-                    <input
-                        type="email" {...register('email')}
-                        className="input"
-                        placeholder="Email" />
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                    <h2 className='text-5xl font-bold'>please Login</h2>
+                    <form onSubmit={handleSubmit(oneSubmit)}>
+                        <fieldset className="fieldset">
+                            <label className="label">Email</label>
+                            <input
+                                type="email" {...register('email')}
+                                className="input"
+                                placeholder="Email" />
 
-                    <label className="label">Password</label>
-                    <input
-                        type="password"
-                        {...register("password", { required: true, minLength: 6 })}
-                        className="input"
-                        placeholder="Password" />
-                    {
-                        errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>
-                    }
-                    {
-                        errors.password?.type === 'minLength' && <p className='text-red-500'>Password must be 6 characters or longer</p>
-                    }
+                            <label className="label">Password</label>
+                            <input
+                                type="password"
+                                {...register("password", { required: true, minLength: 6 })}
+                                className="input"
+                                placeholder="Password" />
+                            {
+                                errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>
+                            }
+                            {
+                                errors.password?.type === 'minLength' && <p className='text-red-500'>Password must be 6 characters or longer</p>
+                            }
 
-                    <div><a className="link link-hover">Forgot password?</a></div>
+                            <div><a className="link link-hover">Forgot password?</a></div>
 
-                   <Link className='text-blue-500 font-bold' to="/register"> register</Link>
-                </fieldset>
-                
- <button className="btn btn-neutral mt-4">Login</button>
-            </form>
+                            
+                            <button className="btn bg-[#CAEB66]  text-black font-bold  mt-4">Register</button>
+                        </fieldset>
+                        <p><small> New to this website? <Link className='text-blue-500 font-bold text-sm' to="/register"> register</Link></small></p>
+                            <SocialLogin></SocialLogin>
+                    
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
         <div>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
-                    <h2 className='text-5xl font-bold'>Create an Account</h2>
+                    <h2 className='text-5xl font-bold'>Create Account</h2>
                     <form onSubmit={handleSubmit(oneSubmit)}>
                         <fieldset className="fieldset">
                             {/* email */}
@@ -53,9 +53,10 @@ const Register = () => {
                             }
 
                             <div><a className="link link-hover">Forgot password?</a></div>
-                            <Link className='text-blue-500 font-bold' to="/login"> login</Link>
-                            <button className="btn btn-neutral mt-4">Register</button>
+                           
+                            <button className="btn bg-[#CAEB66]  text-black font-bold  mt-4">Register</button>
                         </fieldset>
+                        <p><small> Already have an account?  <Link className='text-blue-500 font-bold text-sm' to="/login"> login</Link></small></p>
                     </form>
                 </div>
             </div>
