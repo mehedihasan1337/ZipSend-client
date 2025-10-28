4fc527bc9dd16a1945f052edca58f4bbb483da8f
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2025-10-28 11:37:52 +0600

    added google SocialLogin

--- src/assets/contexts/AuthContext/AuthProvider.jsx
+++ src/assets/contexts/AuthContext/AuthProvider.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../../firebase/firebase.init';
+
+const googleProvider= new GoogleAuthProvider()
+
+
+
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
@@ -14,6 +19,10 @@ const AuthProvider = ({ children }) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
+     const signInWithGoogle=()=>{
+         setLoading(true);
+         return signInWithPopup(auth,googleProvider)
+     }
     const logOut = () => {
         setLoading(true)
         return signOut(false)
@@ -34,6 +43,7 @@ const AuthProvider = ({ children }) => {
         loading,
         createUser,
         signIn,
+        signInWithGoogle,
         logOut,
     }
     return (
